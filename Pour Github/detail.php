c7e17144f793851c0b88229b47fb114458d30d53
AmaraABC <159239691+AmaraABC@users.noreply.github.com>
2024-12-03 06:49:22 -0800

    Add files via upload

---
+++ Pour Github/detail.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr-FR" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site E-commerce en HTML5 & CSS3 - Serveur Web</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header class="website-header">
+        <a href="../index.php">
+            <h1 class="header-logo">Website.</h1>
+        </a>
+        <nav>
+            <a href="catalogue.php">Catalogue</a>
+            <a href="inscription.php">Inscription</a>
+            <a href="connexion.php">Connexion</a>
+            <a href="panier.php">Panier</a>
+        </nav>
+    </header>
+
+    <main class="details-main">
+        <section class="details-section">
+            <article class="details-img">
+                <img src="tulips-7884877_640.jpg" alt="Tulip in a black backdrop">
+            </article>
+            <article class="details-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dolorum consequuntur odio
+                    maxime, eius
+                    labore, suscipit perspiciatis in dignissimos nesciunt dolores nobis quos dolor assumenda. Doloremque
+                    nihil atque
+                    sint assumenda.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dolorum consequuntur odio
+                    maxime, eius
+                    labore, suscipit perspiciatis in dignissimos nesciunt dolores nobis quos dolor assumenda. Doloremque
+                    nihil atque
+                    sint assumenda.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dolorum consequuntur odio
+                    maxime, eius
+                    labore, suscipit perspiciatis in dignissimos nesciunt dolores nobis quos dolor assumenda. Doloremque
+                    nihil atque
+                    sint assumenda.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dolorum consequuntur odio
+                    maxime, eius
+                    labore, suscipit perspiciatis in dignissimos nesciunt dolores nobis quos dolor assumenda. Doloremque
+                    nihil atque
+                    sint assumenda.</p>
+            </article>
+        </section>
+    </main>
+
+    <footer class="website-footer">
+        &copy; <strong>Amâra-Noah - 2024</strong>
+    </footer>
+</body>
---
+++ Pour Github/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="fr-FR" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site E-commerce en HTML5 & CSS3 - Serveur Web</title>
+    <link rel="stylesheet" href="View/style.css">
+</head>
+
+<body>
+    <header class="website-header">
+        <a href="index.php">
+            <h1 class="header-logo">Website.</h1>
+        </a>
+        <nav>
+            <a href="View/catalogue.php">Catalogue</a>
+            <a href="View/inscription.php">Inscription</a>
+            <a href="View/connexion.php">Connexion</a>
+            <a href="View/panier.php">Panier</a>
+        </nav>
+    </header>
+
+    <main class="accueil-main">
+        <section class="main-intro">
+            <h1>Bienvenue sur notre site e-commerce !!</h1>
+            <br>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem rem dolore ex delectus harum animi
+                alias,
+                libero non quae corrupti culpa ipsum quibusdam, nam exercitationem dolor numquam architecto sed
+                deleniti.
+            </p>
+            <br>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dignissimos ex minus veritatis inventore.
+                Voluptates, iure perspiciatis! Debitis quod libero ad consequatur reprehenderit fugiat veritatis, sint
+                assumenda
+                eaque dolores distinctio.
+            </p>
+        </section>
+
+        <hr class="delimiter">
+
+        <section class="section-description">
+            <article class="article-text">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem rem dolore ex delectus harum animi
+                    alias,
+                    libero non quae corrupti culpa ipsum quibusdam, nam exercitationem dolor numquam architecto sed
+                    deleniti.
+                </p>
+                <br>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dignissimos ex minus veritatis
+                    inventore.
+                    Voluptates, iure perspiciatis! Debitis quod libero ad consequatur reprehenderit fugiat veritatis,
+                    sint assumenda
+                    eaque dolores distinctio.
+                </p>
+                <br>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem rem dolore ex delectus harum animi
+                    alias,
+                    libero non quae corrupti culpa ipsum quibusdam, nam exercitationem dolor numquam architecto sed
+                    deleniti.
+                </p>
+            </article>
+            <article class="article-img">
+                <img src="View/tulips-7884877_640.jpg" alt="Tulip in a black backdrop">
+            </article>
+        </section>
+
+        <hr class="delimiter">
+
+        <section class="section-description">
+            <article class="article-img">
+                <img src="View/tulips-7884877_640.jpg" alt="Tulip in a black backdrop">
+            </article>
+            <article class="article-text">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem rem dolore ex delectus harum animi
+                    alias,
+                    libero non quae corrupti culpa ipsum quibusdam, nam exercitationem dolor numquam architecto sed
+                    deleniti.
+                </p>
+                <br>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dignissimos ex minus veritatis
+                    inventore.
+                    Voluptates, iure perspiciatis! Debitis quod libero ad consequatur reprehenderit fugiat veritatis,
+                    sint assumenda
+                    eaque dolores distinctio.
+                </p>
+                <br>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem rem dolore ex delectus harum animi
+                    alias,
+                    libero non quae corrupti culpa ipsum quibusdam, nam exercitationem dolor numquam architecto sed
+                    deleniti.
+                </p>
+            </article>
+        </section>
+    </main>
+
+    <footer class="website-footer">
+        &copy; <strong>Amâra-Noah - 2024</strong>
+    </footer>
+</body>
+
+</html>
